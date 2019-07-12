54bea9148447dbc102439ab7ec018cda0a851a89
robvpsl <robertgomez@villageparkseniorliving.com>
2019-07-12 13:35:07 -0400

    fixing starting script 7

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,9 @@
 cd /home/ec2-user/sample/backend/
 nohup npm start
 yes "" | command
+yes "" | command
 cd /home/ec2-user/sample/frontend
 nohup node server.js
 yes "" | command
+yes "" | command
+exit 0
